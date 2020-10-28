2c70885e12d525a4fe90c713c1cee70b327a45f2
Antonio Soto <antonios754@gmail.com>
2020-10-28 10:07:40 +0100

    Generación de nuevos test unitarios de situaciones de interacción con el usuario

--- src/test/java/training/weather/WeatherForecastTest.java
+++ src/test/java/training/weather/WeatherForecastTest.java
@@ -4,14 +4,64 @@ import java.io.IOException;
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.Date;
+
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
+/**
+ * Pruebas unitarias para la clase WeatherForecast
+ * 
+ * @author Antonio Soto
+ */
 public class WeatherForecastTest {
 
+	/**
+	 * Objeto principal
+	 */
+	private WeatherForecast weatherForecast;
+
+	/**
+	 * Inicialización previa de las variables globales
+	 */
+	@Before
+	public void init() {
+		weatherForecast = new WeatherForecast();
+	}
+
+	/**
+	 * Test unitario de búsqueda del tiempo en una ciudad en una fecha concreta
+	 * 
+	 * @throws IOException Excepción en conexión con API.
+	 */
 	@Test
-	public void unfinished_test() throws IOException {
-		WeatherForecast weatherForecast = new WeatherForecast();
-		String forecast = weatherForecast.getCityWeather("Santander", new Date());
+	public void weatherCityDateTest() throws IOException {
+		String forecast = weatherForecast.getCityWeather("Santander", "31/10/2020");
+		Assert.assertNotNull(forecast);
 		System.out.println(forecast);
 	}
+
+	/**
+	 * Test unitario de búsqueda del tiempo en una ciudad sin fecha
+	 * 
+	 * @throws IOException Excepción en conexión con API.
+	 */
+	@Test
+	public void weatherCityNoDateTest() throws IOException {
+		String forecast = weatherForecast.getCityWeather("Santander", null);
+		Assert.assertNotNull(forecast);
+		System.out.println(forecast);
+	}
+
+	/**
+	 * Test unitario de búsqueda del tiempo en una ciudad con nombre erroneo
+	 * 
+	 * @throws IOException Excepción en conexión con API.
+	 */
+	@Test
+	public void weatherBadCityTest() throws IOException {
+		String forecast = weatherForecast.getCityWeather("Santanderea", "31/10/2020");
+		Assert.assertNull(forecast);
+	}
+
 }
